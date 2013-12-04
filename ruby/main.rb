6e854f894eddce834407bc63f062c47e5e5f25f9
Jake Kaufman <theevocater@gmail.com>
2013-12-04 00:17:06 +0000

    Add random string comparer in ruby

--- ruby/main.rb
+++ ruby/main.rb
@@ -135,3 +135,27 @@ the_string = "hatche"
 puts the_string
 puts MyBase64.encode64(the_string)
 puts MyBase64.decode64(MyBase64.encode64(the_string))
+
+iterations = 100
+iterations.times.map {
+  # found this method when looking up the ruby "random" module
+  str_length = rand(10000).to_i
+  the_string = (36**(str_length-1) + rand(36**str_length)).to_s(36)
+  encoded_mine = MyBase64.encode64(the_string)
+  encoded_lib = Base64.strict_encode64(the_string)
+  if encoded_mine != encoded_lib
+    puts encoded_mine
+    puts encoded_lib
+    raise "Failed to encode " + the_string
+  end
+
+  decoded_mine = MyBase64.decode64(encoded_mine)
+  decoded_lib = Base64.strict_decode64(encoded_lib)
+
+  if decoded_mine != decoded_lib
+    puts encoded_mine
+    puts decoded_mine
+    puts decoded_lib
+    raise "Failed to decode: " + the_string
+  end
+}
